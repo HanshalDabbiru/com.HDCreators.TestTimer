126aa623618d59cfd7ea298f170cc5cc503ba0ad
HanshalDabbiru <hanshal.dabbiru@outlook.com>
2021-10-06 18:39:14 -0700

    Initial Commit

--- app/src/main/java/com/HDCreators/TestTimer/ui/main/MainFragment.java
+++ app/src/main/java/com/HDCreators/TestTimer/ui/main/MainFragment.java
@@ -26,7 +26,12 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.main_fragment, container, false);
+        View rootView = inflater.inflate(R.layout.main_fragment, container, false);
+        View satButton = rootView.findViewById(R.id.SATButton);
+        satButton.setOnClickListener(v -> onSATClick(v));
+        View actButton = rootView.findViewById(R.id.ACTButton);
+        actButton.setOnClickListener(v -> onACTClick(v));
+        return rootView;
     }
 
     @Override
@@ -40,6 +45,13 @@ public class MainFragment extends Fragment {
     {
         view.findViewById(R.id.SATButton);
         Intent intent = new Intent(MainFragment.this.getContext(), SecondFragment.class);
+        MainFragment.this.startActivity(intent);
+    }
+    public void onACTClick(View view)
+    {
+        view.findViewById(R.id.SATButton);
+        Intent intent = new Intent(MainFragment.this.getContext(), SecondFragment.class);
+        MainFragment.this.startActivity(intent);
     }
 
 }
